f0077fae1db6693cd6a3729eda184bdc5139e235
Mauriel <mauriel.azofeifa@uned.cr>
2021-03-31 02:20:21 +0000

    My-work

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, json
+import json
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Paciente, Hospital, Doctor
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +39,114 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Todo lo relacionado con paciente
+@app.route('/paciente', methods=['GET'])
+def get_users():
+    user = Paciente.query.all()
+    mapeo= list(map(lambda x: x.serialize(),user))
+    return jsonify(mapeo), 200
+
+
+@app.route('/paciente/<int:id_get>', methods=['GET'])
+def get_with_id(id_get):
+    usuario = Paciente.query.get(id_get)
+    usuario_final= usuario.serialize()
+    return jsonify(usuario_final), 200
+
+@app.route('/paciente', methods=['POST'])
+def add_user():
+   
+    request_body = json.loads(request.data)
+
+    if request_body["name"] == None and request_body["last_name"] == None and request_body["suffering"] == None:
+        return "Datos incompletos"
+    else:
+
+        user = Paciente(name=request_body["name"], last_name=request_body["last_name"], suffering=request_body["suffering"])
+        db.session.add(user)
+        db.session.commit()
+        return "Posteo exitoso"
+    
+@app.route('/paciente/<int:DNI>', methods=['DELETE'])
+def delete_user_by_id(DNI):
+    user = Paciente.query.filter_by(DNI=DNI).first_or_404()
+    db.session.delete(user)
+    db.session.commit()
+    return("User has been deleted successfully"), 200
+
+# Todo relacionado con doctor
+@app.route('/doctor', methods=['GET'])
+def get_users_doctors():
+    user = Doctor.query.all()
+    mapeo= list(map(lambda x: x.serialize(),user))
+    return jsonify(mapeo), 200
+
+
+@app.route('/doctor/<int:id_get>', methods=['GET'])
+def get_with_id_doctor(id_get):
+    usuario = Doctor.query.get(id_get)
+    usuario_final= usuario.serialize()
+    return jsonify(usuario_final), 200
+
+@app.route('/doctor', methods=['POST'])
+def add_user_doctors():
+   
+    request_body = json.loads(request.data)
+
+    if request_body["name"] == None and request_body["last_name"] == None and request_body["Especialidad"] == None:
+        return "Datos incompletos"
+    else:
+
+        user = Doctor(name=request_body["name"], last_name=request_body["last_name"], Especialidad=request_body["Especialidad"])
+        db.session.add(user)
+        db.session.commit()
+        return "Posteo exitoso"
+    
+@app.route('/doctor/<int:DNI_doctor>', methods=['DELETE'])
+def delete_user_by_id_doctors(DNI_doctor):
+    user = Doctor.query.filter_by(DNI_doctor=DNI_doctor).first_or_404()
+    db.session.delete(user)
+    db.session.commit()
+    return("User has been deleted successfully"), 200
+
+# Todo relacionado con hospital
+
+@app.route('/hospital', methods=['GET'])
+def get_users_hospital():
+    user = Hospital.query.all()
+    mapeo= list(map(lambda x: x.serialize(),user))
+    return jsonify(mapeo), 200
+
+
+@app.route('/hospital/<int:id_get>', methods=['GET'])
+def get_with_id_hospital(id_get):
+    usuario = Hospital.query.get(id_get)
+    usuario_final= usuario.serialize()
+    return jsonify(usuario_final), 200
+
+@app.route('/hospital', methods=['POST'])
+def add_user_hospital():
+   
+    request_body = json.loads(request.data)
+
+    # if request_body["birth"] :
+    #     return "Datos incompletos"
+    # else:
+
+    user = Hospital(birth=request_body["birth"], DNI_paciente_hospital=request_body["DNI_paciente_hospital"], DNI_doctor_hospital=request_body["DNI_doctor_hospital"])
+    db.session.add(user)
+    db.session.commit()
+    return "Posteo exitoso"
+    
+@app.route('/hospital/<int:DNI_cita>', methods=['DELETE'])
+def delete_user_by_id_hospital(DNI_cita):
+    user = Hospital.query.filter_by(DNI_cita=DNI_cita).first_or_404()
+    db.session.delete(user)
+    db.session.commit()
+    return("User has been deleted successfully"), 200
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,66 @@
 from flask_sqlalchemy import SQLAlchemy
-
+import os
+from flask_admin import Admin
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DATETIME
+from flask_admin.contrib.sqla import ModelView
+from datetime import datetime, timezone, timedelta
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Paciente(db.Model):
+    DNI = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    suffering = db.Column(db.String(80), unique=False, nullable=False)
+   
+    # favorites= db.relationship('Favorites', lazy=True)
+    # def __repr__(self):
+    #     return '<User %r>' % self.name 
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
+            "DNI": self.DNI,
+            "name": self.name,
+            "last_name": self.last_name,
+            "suffering": self.suffering,
+          
+          
+            
+            # do not serialize the password, its a security breach
+        }
+class Doctor(db.Model):
+    DNI_doctor = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    Especialidad = db.Column(db.String(120), unique=False, nullable=False)
+    
+   
+    # DNI_doctor_hospital = db.Column(db.Integer, db.ForeignKey(Paciente.DNI))
+
+    def serialize(self):
+        return {
+            "DNI_doctor": self.DNI_doctor,
+            "name": self.name,
+            "last_name": self.last_name,
+            "Especialidad": self.Especialidad,
+            # "type": self.type,
+            # do not serialize the password, its a security breach
+        }
+       
+     
+        
+class Hospital(db.Model):
+    DNI_cita = db.Column(db.Integer, primary_key=True)
+    birth = db.Column(DATETIME)
+    DNI_paciente_hospital = db.Column(db.Integer, db.ForeignKey(Paciente.DNI))
+    DNI_doctor_hospital = db.Column(db.Integer, db.ForeignKey(Doctor.DNI_doctor))
+    # DNI_doctor_hospital = db.Column(db.Integer, db.ForeignKey(Paciente.DNI))
+
+    def serialize(self):
+        return {
+            "DNI_cita": self.DNI_cita,
+            "birth": self.birth,
+            "DNI_paciente_hospital": self.DNI_paciente_hospital,
+            "DNI_doctor_hospital": self. DNI_doctor_hospital,
+            # "type": self.type,
             # do not serialize the password, its a security breach
         }
